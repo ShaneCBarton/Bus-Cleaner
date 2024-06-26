aaab2b9d20e5d651982e96774dc53b8d9546897b
Shane Barton <shanecb@hotmail.ca>
2024-06-26 12:11:49 -0400

    all trash is working as it should be and collectable

---
+++ Assets/Scripts/DebugInventory.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DebugInventory : MonoBehaviour
+{
+    [SerializeField] private PlayerInventory inventory;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            inventory.PickedUpItem(ItemID.SoapBucket);
+        }
+        else if (Input.GetKeyDown(KeyCode.Y)) 
+        {
+            inventory.PickedUpItem(ItemID.TrashBag);
+        }
+        else if (Input.GetKeyDown(KeyCode.U)) 
+        {
+            inventory.PickedUpItem(ItemID.Vaccuum);
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventory.PickedUpItem(ItemID.HotWaterBucket);
+        }
+    }
+}
